2e3c7dd2178f362ecb2e97081af60503ada71746
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-09-24 18:11:18 +0300

    test(products): validate UUID format in product addition tests
    - Added Zod schema validation for UUID format in the product addition tests.
    - Enhanced the successful product addition test to validate the UUID of the returned product ID.
    - Retained the existing validation error tests for invalid product input.

--- src/tests/productsRoutes.test.ts
+++ src/tests/productsRoutes.test.ts
@@ -2,6 +2,9 @@ import { describe, expect, it } from 'vitest';
 import request from 'supertest';
 import { Product } from '../models/Product';
 import app from '../app';
+import { z } from 'zod';
+
+const uuidSchema = z.string().uuid();
 
 describe('GET /api/products', () => {
 	it('should return an empty array', async () => {
@@ -15,5 +18,48 @@ describe('GET /api/products', () => {
 		expect(response.status).toBe(200);
 		expect(response.body).toEqual(emptyArray);
 		expect(response.body).toHaveLength(0);
+	});
+});
+
+describe('POST /api/products', () => {
+	it('should add a product', async () => {
+		// Arrange
+		const product: Product = {
+			name: 'Product 1',
+			price: 10,
+			stock: 10,
+		};
+
+		// Act
+		const response = await request(app).post('/api/products').send(product);
+
+		// Assert
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty('id');
+		expect(uuidSchema.safeParse(response.body.id).success).toBe(true);
+		expect(response.body).toMatchObject(product);
+	});
+
+	it('should return a 422 error if the product is invalid', async () => {
+		// Arrange
+		const product: Product = {
+			name: 'P',
+			price: -10,
+			stock: -10,
+		};
+		const errors = [
+			'body.name Name must be at least 2 characters long.',
+			'body.price Number must be greater than 0',
+			'body.stock Number must be greater than or equal to 0',
+		];
+
+		// Act
+		const response = await request(app).post('/api/products').send(product);
+
+		// Assert
+		expect(response.status).toBe(422);
+		expect(response.body).toHaveProperty('message');
+		expect(response.body.status).toBe('fail');
+		expect(response.body.errors).toEqual(errors);
 	});
 });
